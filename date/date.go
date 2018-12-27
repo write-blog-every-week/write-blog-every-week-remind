9e47c9598d0f0c11c3229be93193076a0efdad71
kojirock <kojirock5260@gmail.com>
2018-12-27 21:42:14 +0900

    日付のstub関数を作成

--- date/date.go
+++ date/date.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+var fakeTime time.Time
+
 // GetWeekDayNumber 曜日の番号を返す
 func GetWeekDayNumber() int {
 	// Goの場合
@@ -12,7 +14,7 @@ func GetWeekDayNumber() int {
 	// になるので、Pythonにあわすため、以下にする
 	// 0 => 月曜
 	// 6 => 日曜
-	weekday := int(time.Now().Weekday()) - 1
+	weekday := int(TimeNow().Weekday()) - 1
 	if weekday == -1 {
 		weekday = 6
 	}
@@ -34,10 +36,28 @@ func GetLastWeekMonday() time.Time {
 	return nowDate.AddDate(0, 0, -weekday)
 }
 
+// SetFakeTime stub用の日付データをセットする
+func SetFakeTime(t time.Time) {
+	fakeTime = t
+}
+
+// ResetFake stubとして設定した日付データをリセットする
+func ResetFake() {
+	fakeTime = time.Time{}
+}
+
+// TimeNow 現在の日付を返す(stub用のデータが有る場合はstubデータを返す)
+func TimeNow() time.Time {
+	if !fakeTime.IsZero() {
+		return fakeTime
+	}
+	return time.Now()
+}
+
 /**
  * 現在の日付を取得する
  */
 func getNowDate() time.Time {
-	t := time.Now()
+	t := TimeNow()
 	return time.Date(t.Year(), t.Month(), t.Day(), 00, 00, 00, 0, time.Local)
 }
